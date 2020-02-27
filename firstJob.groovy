31a33700ef57c39edf15185c52cb2e3c2b4084f5
fhorazny <60750326+fhorazny@users.noreply.github.com>
2020-02-27 15:25:55 +0100

    Update firstJob.groovy

--- firstJob.groovy
+++ firstJob.groovy
@@ -1,4 +1,4 @@
-pipelinejob('firstTry') {
+pipelineJob('firstTry') {
     description 'useless job, only calling some shell commands, when i figure out how to upload sources, will try building java.'
     label('rvais-openshift-testrunner')
     cpsScm {
