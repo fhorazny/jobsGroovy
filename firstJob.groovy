e0fe71348d202b6261afe92c699459d27380d5bc
František Horázný <fhorazny@redhat.com>
2020-02-27 14:11:59 +0100

    NEW: groovy script for seedJob, groovy script for first useless job

---
+++ firstJob.groovy
@@ -0,0 +1,31 @@
+job('firstTry') {
+    description 'useless job, only calling some shell commands, when i figure out how to upload sources, will try building java.'
+    label('rvais-openshift-testrunner')
+    scm {
+        git {
+            branch('*/master')
+            remote{
+                credentials('buildadm-ccikey')
+                github('com:fhorazny/uselessCalc.git')
+            }
+        }
+    }
+
+    steps {
+        maven {
+            goals 'clean package'
+        }
+        shell('''java -jar target/first-0.0.1-SNAPSHOT.jar
+
+RANDOM=$$
+R=$(($RANDOM%5))
+echo $R
+if [ "$R" = "1" ]
+then
+    echo "not Cool"
+    exit 1
+else
+    echo "Cool"
+fi''')
+    }
+}
---
+++ seedJob.groovy
@@ -0,0 +1,59 @@
+// ######## Views definitions ########
+// ###################################
+sectionedView('fhoraznySeedExperiment') {
+description('Automated jobs for fhorazny')
+sections {
+    listView {
+        name("fhoraznySeedJobed")
+        alignment('LEFT')
+        jobs {
+            name("firstJob")
+        }
+        columns {
+            status()
+            weather()
+            name()
+            lastSuccess()
+            lastFailure()
+            lastDuration()
+            buildButton()
+            lastBuildConsole()
+        }
+    }
+    /*listView {
+        name("another view")
+        alignment('LEFT')
+        jobs {
+            regex("amq7-broker-ocp.*")
+            regex("amq7-broker-ocp-*")
+        }
+        columns {
+            status()
+            weather()
+            name()
+            lastSuccess()
+            lastFailure()
+            lastDuration()
+            buildButton()
+            lastBuildConsole()
+        }
+    }*/
+}
+}
+
+// ######### Job definitions #########
+// ###################################
+
+
+
+pipelineJob('firstJob') {
+    parameters {
+        stringParam('FIRSTPARAMETER', 'DEFAULT', 'DESCRIPTION')
+    }
+    definition {
+        cps {
+            script(readFileFromWorkspace('firstjob.groovy'))
+            sandbox()
+        }
+    }
+}
